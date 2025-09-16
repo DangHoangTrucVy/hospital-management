18e64d44dfd211e69fe9b58c04d0a4339f76b18f
DangHoangTrucVy <danghoangtrucvy090105@gmail.com>
2025-09-17 00:38:44 +0700

    Update PatientRepository.java

--- PatientRepository.java
+++ PatientRepository.java
@@ -1,14 +1,10 @@
+package com.project.backend.repositories;
 
-package com.project.back_end.repositories;
-
-import com.project.back_end.models.Patient;
+import com.project.backend.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-    // JpaRepository đã cung cấp các phương thức CRUD cơ bản
-    // Bạn có thể thêm các truy vấn tùy chỉnh nếu cần, ví dụ:
-
-    // Optional<Patient> findByEmail(String email);
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByEmail(String email); // tìm patient theo email, ví dụ login
 }
