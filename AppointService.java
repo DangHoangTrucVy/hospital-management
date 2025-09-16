11993b276a76d6c27f61c8d7a08ab0f18a5fd6ea
DangHoangTrucVy <danghoangtrucvy090105@gmail.com>
2025-09-17 00:41:09 +0700

    Update AppointService.java

--- AppointService.java
+++ AppointService.java
@@ -1,63 +1,32 @@
-package com.project.back_end.services;
+package com.project.backend.services;
 
-import com.project.back_end.models.Appointment;
+import com.project.backend.models.Appointment;
+import com.project.backend.repositories.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class AppointmentService {
 
-    private List<Appointment> appointmentList = new ArrayList<>();
+    @Autowired
+    private AppointmentRepository appointmentRepository;
 
-    // Get all appointments
     public List<Appointment> getAllAppointments() {
-        return appointmentList;
+        return appointmentRepository.findAll();
     }
 
-    // Get appointment by ID
-    public Appointment getAppointmentById(int id) {
-        return appointmentList.stream()
-                .filter(a -> a.getId() == id)
-                .findFirst()
-                .orElse(null);
+    public Optional<Appointment> getAppointmentById(Long id) {
+        return appointmentRepository.findById(id);
     }
 
-    // Get appointments by patient ID
-    public List<Appointment> getAppointmentsByPatientId(int patientId) {
-        return appointmentList.stream()
-                .filter(a -> a.getPatientId() == patientId)
-                .collect(Collectors.toList());
+    public Appointment createAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
     }
 
-    // Get appointments by doctor ID
-    public List<Appointment> getAppointmentsByDoctorId(int doctorId) {
-        return appointmentList.stream()
-                .filter(a -> a.getDoctorId() == doctorId)
-                .collect(Collectors.toList());
-    }
-
-    // Add new appointment
-    public Appointment addAppointment(Appointment appointment) {
-        appointmentList.add(appointment);
-        return appointment;
-    }
-
-    // Update an appointment
-    public Appointment updateAppointment(int id, Appointment updatedAppointment) {
-        for (int i = 0; i < appointmentList.size(); i++) {
-            if (appointmentList.get(i).getId() == id) {
-                appointmentList.set(i, updatedAppointment);
-                return updatedAppointment;
-            }
-        }
-        return null;
-    }
-
-    // Delete an appointment
-    public boolean deleteAppointment(int id) {
-        return appointmentList.removeIf(a -> a.getId() == id);
+    public void deleteAppointment(Long id) {
+        appointmentRepository.deleteById(id);
     }
 }
